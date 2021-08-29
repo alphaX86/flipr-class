6da5e0ca9d6d5c015c7d34fd755dd5d2701d7c2e
Aadhitya A <aadhitya864@gmail.com>
2021-08-29 16:23:19 +0530

    Add blank pages
    Signed-off-by: Aadhitya A <aadhitya864@gmail.com>

--- frontend/src/pages/Studentlogin.js
+++ frontend/src/pages/Studentlogin.js
@@ -1,4 +1,17 @@
 import React from "react";
+
+async function main() {
+    
+    const { MongoClient } = require('mongodb');
+    const uri = "mongodb+srv://admin:Aadhi#1234@cluster-flipr.vds6n.mongodb.net/flipr_class?retryWrites=true&w=majority";
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+    const collection = client.db("flipr_class").collection("students");
+    // perform actions on the collection object
+    client.close();
+    });
+
+}
 class StudentLogin extends React.Component {
     render() {
         return (
